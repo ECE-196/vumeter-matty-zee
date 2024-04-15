5a309333afa182d50b173c46bee178398fa0aeb9
matty-zee <97648068+matty-zee@users.noreply.github.com>
2024-04-15 15:12:05 -0700

    finishing extra credit solution

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#EXTRA CREDIT SOLUTION
 import board
 from digitalio import DigitalInOut, Direction
 from analogio import AnalogIn
@@ -5,33 +6,44 @@ from time import sleep
 
 # setup pins
 microphone = AnalogIn(board.IO1)
-
 status = DigitalInOut(board.IO17)
 status.direction = Direction.OUTPUT
 
 led_pins = [
     board.IO21,
-    board.IO26, # type: ignore
+    board.IO26,  # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33,
+    board.IO34,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
-
 for led in leds:
     led.direction = Direction.OUTPUT
 
-# main loop
+decay_rate = 0.1  
+
 while True:
     volume = microphone.value
+    volume_percent = int((volume - 25000) / 25000 * 100)
+    if volume_percent < 0:
+        volume_percent = volume_percent * -1
 
-    print(volume)
+    # print(volume_percent)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    target_num_leds_on = volume_percent // 11
 
-    sleep(1)
-
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    for i in range(len(leds)):
+        if i < target_num_leds_on:
+            leds[i].value = True
+        else:
+            for j in range(len(leds)-1, 0, -1):
+                if leds[j].value:
+                    leds[j].value = False
+                    sleep(decay_rate)  
